fcfb592d4ac82ea093bfa4de3033139bdcc56a85
Victor Monnot <victormonnot@protonmail.com>
2026-01-12 22:19:40 +0100

    fix: removed a print function

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(00, 100):
-    if i < 10:
-        print("0{},".format(i), end=" ")
+    if i < 99:
+        print("{:02},".format(i), end=" ")
     elif i == 99:
         print("{}".format(i))
-    elif i > 10:
-        print("{},".format(i), end=" ")
