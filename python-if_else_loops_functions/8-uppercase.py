613bbd6a6fbb8dcf5cdf89564feaa93dff621c63
Victor Monnot <victormonnot@protonmail.com>
2026-01-13 10:37:06 +0100

    feat: Created a python function that prints a string in uppercase followed by a new line.

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,8 +5,6 @@ def uppercase(str):
         if 97 <= ord(i) <= 122:
             i = ord(i) - 32
             print("{}".format(chr(i)), end="")
-#        elif i == str[0:]:
-#           print('\n')
         else:
             print("{}".format(i), end="")
     print()
