b3f6999f4d8133586252619a1c89b7eb32272bcc
Victor Monnot <victormonnot@protonmail.com>
2026-01-13 11:05:17 +0100

    fix: added a way to add a new line at the end of the string

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def uppercase(str):
+    str = str + '\n'
     for i in str:
         if 97 <= ord(i) <= 122:
             i = ord(i) - 32
             print("{}".format(chr(i)), end="")
         else:
             print("{}".format(i), end="")
-    print()
