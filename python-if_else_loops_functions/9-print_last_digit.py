f38eb55d3a8e7de2e8431178c0a96ec832b2b1c1
Victor Monnot <victormonnot@protonmail.com>
2026-01-13 14:15:43 +0100

    feat: Created a Python function that prints the last digit of a number.

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    last_digit = number % 10
+    
+    if number >= 0:
+        print(f"{last_digit}", end="")
+    if number < 0:
+        last_digit = -number % 10
+        print(f"{last_digit}", end="")
+    return last_digit
