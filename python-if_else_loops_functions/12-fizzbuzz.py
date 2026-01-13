09b6f9ad9ddc44ed1fef09b60f87b15da7d03a7a
Victor Monnot <victormonnot@protonmail.com>
2026-01-13 15:07:34 +0100

    fix: added the function Prototype

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-#def fizzbuzz():
+def fizzbuzz():
 
-for i in range (1,101):
-    if i % 3 == 0 and i % 5 == 0:
-        print(f"FizzBuzz ", end = "")
-    elif i % 3 == 0:
-        print(f"Fizz ", end = "")
-    elif i % 5 == 0:
-        print(f"Buzz ", end = "")
-    else:
-        print(f"{i} ", end = "")
+    for i in range (1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print(f"FizzBuzz ", end = "")
+        elif i % 3 == 0:
+            print(f"Fizz ", end = "")
+        elif i % 5 == 0:
+            print(f"Buzz ", end = "")
+        else:
+            print(f"{i} ", end = "")
