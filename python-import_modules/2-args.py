9f69ee4b2832a519b678fdd2401208a3fbc1c2ac
Victor Monnot <victormonnot@protonmail.com>
2026-01-14 11:03:42 +0100

    feat: Created a Python program that prints the number of and the list of its arguments.

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print(f"{argc} argument:")
+        print(f"1: {sys.argv[1]}")
+    else:
+        print(f"{argc} arguments:")
+        for i in range(1,argc + 1):
+            print(f"{i}: {sys.argv[i]}")
