8aba8a52aaf43392d8fdfa23803d0c6d799a0845
Victor Monnot <victormonnot@protonmail.com>
2026-01-19 15:32:59 +0100

    fix: changed .format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in range(len(i)):
             if j == len(i) - 1:
-                print("{:d}".format(j), end = "")
+                print("{:d}".format(i[j]), end = "")
             else:
-                print("{:d}".format(j), end = " ")
+                print("{:d}".format(i[j]), end = " ")
         print()
