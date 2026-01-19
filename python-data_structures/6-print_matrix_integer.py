059dfcdbde8a925de3b2a7c7a279b6c9e9e0002b
Victor Monnot <victormonnot@protonmail.com>
2026-01-19 14:57:12 +0100

    feat: Created a Python function that prints a matrix of integers.

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,9 @@
 
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in i:
-            print("{:d}".format(j), end = " ")
+        for j in range(len(i)):
+            if j == len(i) - 1:
+                print("{:d}".format(j), end = "")
+            else:
+                print("{:d}".format(j), end = " ")
         print()
