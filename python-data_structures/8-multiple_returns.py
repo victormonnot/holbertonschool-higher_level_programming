a9e880006ee1e2e11ead145e94601db623df8efb
Victor Monnot <victormonnot@protonmail.com>
2026-01-19 16:04:52 +0100

    fix: changed lines order

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    length = len(sentence)
-    first_character = sentence[0]
     if sentence == "":
         return (0, None)
+    length = len(sentence)
+    first_character = sentence[0]
     return (length, first_character)
