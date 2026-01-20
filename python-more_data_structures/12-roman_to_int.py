eb9a9832d1b5e26909cfa13d0b5fe2dbc95a3cfc
Victor Monnot <victormonnot@protonmail.com>
2026-01-20 15:17:37 +0100

    feat: Created a Python function that converts a Roman numeral to an integer.

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    number = 0
+    roman_dict = {'I': 1,
+                  'V': 5,
+                  'X': 10,
+                  'L': 50,
+                  'C': 100,
+                  'D': 500,
+                  'M': 1000}
+
+    for i in range(1, len(roman_string)):
+        if roman_dict[roman_string[i]] <= roman_dict[roman_string[i-1]]:
+            number += roman_dict[roman_string[i-1]]
+        else:
+            number -= roman_dict[roman_string[i-1]]
+    number += roman_dict[roman_string[-1]]
+    return number
