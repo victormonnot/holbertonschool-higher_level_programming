012610e2ded2dee99a02cc33465ac0ce14a435d9
Victor Monnot <victormonnot@protonmail.com>
2026-01-23 14:49:17 +0100

    fix: changed the float overflow raise

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,16 +5,20 @@ Module that defines add_integer function
 
 
 def add_integer(a, b=98):
-    """
-    Adds two integers.
-    """
-
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
         raise TypeError("b must be an integer")
 
     try:
-        return int(a) + int(b)
+        a = int(a)
     except OverflowError:
         raise TypeError("a must be an integer")
+
+    try:
+        b = int(b)
+    except OverflowError:
+        raise TypeError("b must be an integer")
+
+    return a + b
+
