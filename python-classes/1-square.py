4e01c8df7e697e98c9f3d16163460a4acdbcb578
Victor Monnot <victormonnot@protonmail.com>
2026-01-29 11:10:56 +0100

    feat: Created a class Square that defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defines a Square class
+"""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size):
+        """Initialize square with size"""
+        self.__size = size
