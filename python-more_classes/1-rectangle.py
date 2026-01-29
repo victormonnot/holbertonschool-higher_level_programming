496879d21e08cd402ebc76483e2a5bf2681d2180
Victor Monnot <victormonnot@protonmail.com>
2026-01-29 16:35:56 +0100

    feat: Created a class Rectangle that defines a rectangle

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class"""
+
+
+class Rectangle:
+    """Rectangle with width and height"""
+
+    def __init__(self, width=0, height=0):
+        """Initialization"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter function for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter function for width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter function for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter function for height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
