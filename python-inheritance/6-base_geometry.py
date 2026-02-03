2eaa3a2b73e08e6e75ba8442b33b1d14ff6a8d6d
Victor Monnot <victormonnot@protonmail.com>
2026-02-03 15:25:23 +0100

    feat: Created an instance that raise an Exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''base_geometry'''
+
+
+class BaseGeometry():
+    '''Empty Class'''
+    
+    def area(self):
+        """raise Exception"""
+        raise Exception("area() is not implemented")
