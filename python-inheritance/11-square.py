5c2bd1634fb4b2d0eb314502f4d68fbc0b14e794
Victor Monnot <victormonnot@protonmail.com>
2026-02-03 17:44:44 +0100

    feat: Created a class Square that inherits from Rectangle

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''square'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+class Square(Rectangle):
+    '''Square Class'''
+    def __init__(self, size):
+        '''.'''
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        '''.'''
+        return f"[Square] {self.__size}/{self.__size}"
