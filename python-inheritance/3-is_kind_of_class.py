17b2972321e7ec68ba910490e584b35ad467cdd6
Victor Monnot <victormonnot@protonmail.com>
2026-02-03 15:03:47 +0100

    feat: Created a Pyhton function that returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False.

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''file'''
+
+
+def is_same_class(obj, a_class):
+    """Returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False."""
+    if isinstance(obj, a_class):
+        return True
+    if type(obj) is not a_class:
+        return False
