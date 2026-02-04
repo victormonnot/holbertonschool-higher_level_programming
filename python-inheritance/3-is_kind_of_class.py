5cfe8807ae69a98bab3fc1b6b7759225e3add8d7
Victor Monnot <victormonnot@protonmail.com>
2026-02-04 09:22:14 +0100

    fix: Changed an if for an else statement

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,5 +6,5 @@ def is_same_class(obj, a_class):
     """Returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False."""
     if isinstance(obj, a_class):
         return True
-    if type(obj) is not a_class:
+    else:
         return False
--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,5 +6,5 @@ def is_same_class(obj, a_class):
     """Returns True or False"""
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
-    if type(obj) is not a_class:
+    else:
         return False
