7b4702fcaf955591eb3e8f5d1cc6b4ee38666942
Victor Monnot <victormonnot@protonmail.com>
2026-02-04 09:28:00 +0100

    fix: Shortened comment

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,7 +3,7 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """Returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False."""
+    """Return True or False"""
     if isinstance(obj, a_class):
         return True
     else:
