99c2a0ad93546606ae3318ec5fe2e9f4db6e5a21
Victor Monnot <victormonnot@protonmail.com>
2026-02-06 23:49:44 +0100

    feat: Created a wrapper iterator

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''file'''
+
+
+
+class CountedIterator:
+    '''Class'''
+
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        item = next(self.iterator)
+        self.count += 1
+        return item
+
+    def get_count(self):
+        return self.count
