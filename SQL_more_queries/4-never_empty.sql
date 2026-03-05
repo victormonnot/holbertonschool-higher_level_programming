2e268a7d04777d4d9cc2565a61a6e6ef09e1336a
Victor Monnot <victormonnot@protonmail.com>
2026-03-05 16:38:25 +0100

    feat: Created all the SQL scripts for the More SQL project

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- Create table id_not_null with default value 1 for id
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Create database hbtn_0d_usa and table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
